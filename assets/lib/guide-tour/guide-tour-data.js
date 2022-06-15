1476b319e701d6de2b853928321b8f3f421b07f2
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2022-06-15 16:00:41 +0530

    some more changes

--- assets/lib/guide-tour/guide-tour-data.js
+++ assets/lib/guide-tour/guide-tour-data.js
@@ -1,8 +1,8 @@
 const g_tour_steps = [
-    // {
-    //     sourceElmIdQS: "#btn_info",
-    //     informationText: "Instructions - Describes the experiment.",
-    // },
+    {
+        sourceElmIdQS: ".popup_btn",
+        informationText: "Toggle Button - Toggle the properties panel.",
+    },
     {
         sourceElmIdQS: "#btn_themes",
         informationText: "Theme - Provides different user interface colour themes.",
